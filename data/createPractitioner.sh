fa938ce69c3a10ee1764589c8aa153b920476afa
Harsha Talkad <htalkad@salesforce.com>
2021-08-29 21:25:55 -0700

    Cleanup 1

--- data/createPractitioner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sed "s/\${npi}/$1/g" ./template/practitioner.template.json > "/tmp/req.json"
-
-sed -i'.original' -e "s/\${name}/$2/g" "/tmp/req.json"
-sed -i'.original' -e "s/\${familyname}/$3/g" "/tmp/req.json"
-sed -i'.original' -e "s/\${gender}/$4/g" "/tmp/req.json"
-
-practitionerId=$(curl -s -X POST -H "Content-Type: application/fhir+json" -d @/tmp/req.json http://localhost:8090/fhir/Practitioner | jq -r '.id')
-echo 'Practitioner' $practitionerId 'created'
